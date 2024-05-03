1c2f4e206e7f03a450be19c55e5b1b1da4b7ce0b
awilson-dev <awilson-dev@tech-center.com>
2024-05-03 16:15:21 -0400

    Initial Commit

---
+++ Bible Jeopardy (iPad)/Bible_Jeopardy__iPad_App.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_Jeopardy__iPad_App.swift
+//  Bible Jeopardy (iPad)
+//
+//  Created by Allen Wilson on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Bible_Jeopardy__iPad_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
